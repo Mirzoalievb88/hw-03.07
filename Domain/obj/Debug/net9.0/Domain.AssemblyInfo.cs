d61f604881c141038844a93d301bbd35611f7b7b
Mirzoalievb88 <bahodurmirzoaliev2008@gmail.com>
2025-07-05 15:17:18 +0500

    third commit

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55869a08caa5a340b18ad7a892f0f2ba6e6290e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0da773bf7aeddb7b03cfbf23b9ffa5e2677dee76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
